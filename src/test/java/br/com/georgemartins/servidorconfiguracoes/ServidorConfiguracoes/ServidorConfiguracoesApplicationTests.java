5cf317d2f1ecec68a28d33ea4a02ddb5f2ee5e0a
George Martins <george.martins@cwi.com.br>
2023-04-06 18:20:30 -0300

    Commit inicial servidor de configuracao Spring Cloud Config

---
+++ src/test/java/br/com/georgemartins/servidorconfiguracoes/ServidorConfiguracoes/ServidorConfiguracoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.georgemartins.servidorconfiguracoes.ServidorConfiguracoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorConfiguracoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
